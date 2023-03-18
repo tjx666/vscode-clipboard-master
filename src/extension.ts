9815b4d117db5f260c88ec0fd33c4728a443a318
changfeng <changfeng@gaoding.com>
2023-03-19 04:49:36 +0800

    fix: onDidChangeConfiguration function call

--- src/extension.ts
+++ src/extension.ts
@@ -3,38 +3,50 @@ import vscode from 'vscode';
 import configuration from './utils/configuration';
 
 export async function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(
-        vscode.commands.registerTextEditorCommand(
-            'clipboardMaster.copyWithLineNumber',
-            (editor) => {
-                import('./features/copyWithLineNumber').then((mod) =>
-                    mod.copyWithLineNumber(editor),
-                );
-            },
-        ),
-        vscode.commands.registerTextEditorCommand(
-            'clipboardMaster.copyTextWithoutSyntax',
-            (editor) => {
-                import('./features/copyTextWithoutSyntax').then((mod) =>
-                    mod.copyTextWithoutSyntax(editor),
-                );
-            },
-        ),
-        vscode.commands.registerTextEditorCommand('clipboardMaster.smartCopy', (editor) => {
-            import('./features/smartCopy').then((mod) => mod.smartCopy(editor));
-        }),
-        vscode.commands.registerTextEditorCommand(
-            'clipboardMaster.copyAsMarkdownCodeBlock',
-            (editor) => {
-                import('./features/copyAsMarkdownCodeBlock').then((mod) =>
-                    mod.copyAsMarkdownCodeBlock(editor),
-                );
-            },
-        ),
-    );
+    const { commands } = vscode;
+
+    const registerTextEditorCommand = (
+        commandName: string,
+        callback: (
+            textEditor: vscode.TextEditor,
+            edit: vscode.TextEditorEdit,
+            ...args: any[]
+        ) => void,
+        thisArg?: any,
+    ) => {
+        const cmd = commands.registerTextEditorCommand(
+            `clipboardMaster.${commandName}`,
+            callback,
+            thisArg,
+        );
+        context.subscriptions.push(cmd);
+        return cmd;
+    };
+
+    registerTextEditorCommand('copyWithLineNumber', (editor) => {
+        import('./features/copyWithLineNumber').then((mod) => mod.copyWithLineNumber(editor));
+    });
+
+    registerTextEditorCommand('copyTextWithoutSyntax', (editor) => {
+        import('./features/copyTextWithoutSyntax').then((mod) => mod.copyTextWithoutSyntax(editor));
+    });
+
+    registerTextEditorCommand('smartCopy', (editor) => {
+        import('./features/smartCopy').then((mod) => mod.smartCopy(editor));
+    });
+
+    registerTextEditorCommand('copyAsMarkdownCodeBlock', (editor) => {
+        import('./features/copyAsMarkdownCodeBlock').then((mod) =>
+            mod.copyAsMarkdownCodeBlock(editor),
+        );
+    });
 
     configuration.update(context);
-    vscode.workspace.onDidChangeConfiguration(() => {
-        configuration.update(context);
-    }, context.subscriptions);
+    vscode.workspace.onDidChangeConfiguration(
+        () => {
+            configuration.update(context);
+        },
+        null,
+        context.subscriptions,
+    );
 }
